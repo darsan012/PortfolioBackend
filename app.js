ebdacc9ebeae6181acb717a1fed00fe5aa242af5
Darshan Gautam <dsrsangautam@gmail.com>
2022-04-18 13:43:04 +0545

    use of env for getting port information

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log('Time: ',Date.now());
-    next();
-})
+// returns result if the page is refreshed
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
 
-app.get('/',(req,res)=>{
-    res.send('Successful response.');
-})
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
 
-app.listen(2000,()=>console.log("listening on port 3000"))
+// app.listen(2000,()=>console.log("listening on port 3000"))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
