1463246a0506d1263f497930806da8d873dfebbf
Darshan Gautam <dsrsangautam@gmail.com>
2022-04-18 16:33:27 +0545

    added dotenv package and connected to the atlas server

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+require("dotenv/config");
 const express = require("express");
 const res = require("express/lib/response");
+const mongoose = require("mongoose");
+
+const connectionString = process.env.CONNECTION_STRING_ATLAS;
+
 const courses = require("./src/routes/route");
 const app = express();
 
 app.use(express.json());
 app.use("/api/courses", courses);
-
+// console.log(process.env.CONNECTION_STRING_ATLAS);
+// database connection
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("connection sucess .....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
