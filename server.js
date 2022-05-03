31487de51d9179992590f30f457b29cb469510e8
Darshan Gautam <dsrsangautam@gmail.com>
2022-05-03 10:49:53 +0545

    change the origin link on server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 //cors are used for connecting to the react app
 const corsOptions = {
-  origin: "http://localhost:3000/",
+  origin: "https://portfoliodarshan.herokuapp.com",
   optionsSuccessStatus: 200,
 };
 
